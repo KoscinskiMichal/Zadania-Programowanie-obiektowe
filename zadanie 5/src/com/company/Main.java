ca994b4addae2e8e0f7ef6cf76427af2a78126bd
KoscinskiMichal <100791701+KoscinskiMichal@users.noreply.github.com>
2022-04-06 15:31:35 +0200

    zadanie 5/lab

---
+++ zadanie 5/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Prostokat p = new Prostokat(2,3);
+
+        p.pole();
+        p.obwod();
+        p.przekatna();
+
+
+
+    }
+
+
+}
