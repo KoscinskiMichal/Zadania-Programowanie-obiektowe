b15814047f1c3e73fcecef5aa431d978e57a6446
KoscinskiMichal <mk122945@stud.ur.edu.pl>
2022-05-06 13:46:27 +0200

    zadanie 7.2

---
+++ zadanie 7.2/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Wieloryb pletwal = new Wieloryb("Płetwal");
+        pletwal.plyn();
+        pletwal.jedz();
+        pletwal.wynurz();
+        pletwal.zanurz();
+        pletwal.wydalaj();
+
+    }
+}
---
+++ zadanie 7.2/src/com/company/Plywanie.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface Plywanie {
+
+    void plyn();
+    void wynurz();
+    void zanurz();
+
+
+
+}
