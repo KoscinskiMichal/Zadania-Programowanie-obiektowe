6a6de83a0734bd3e783d3371b16aaa60ac2d034f
KoscinskiMichal <mk122945@stud.ur.edu.pl>
2022-06-07 20:42:22 +0200

    zadanie 10.1

---
+++ zadanie 10.1/src/com/company/Kangur.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Kangur {
+    int nrKangura;
+
+    public Kangur(int nrKangura) {
+        this.nrKangura = nrKangura;
+    }
+
+
+    public String skok(){
+        return "Kangur nr"+nrKangura+ " skacze" ;
+    }
+}
--- zadanie 10.1/src/com/company/Main.java
+++ zadanie 10.1/src/com/company/Main.java
@@ -1,29 +1,52 @@
 package com.company;
 
-
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Thriller deathnote = new Thriller(14,"Death Note",false);
-        Horror pila = new Horror(2,"Piła",false);
-        Thriller gurren = new Thriller(2,"Gurren Lagann",false);
-        Horror aot = new Horror(40,"Shingeki na kyiojin",true);
-        Horror paprika = new Horror(2,"Paprika",false);
-        Map<Integer,Film> filmy = new HashMap<>();
-        filmy.put(1,deathnote);
-        filmy.put(2,pila);
-        filmy.put(3,gurren);
-        filmy.put(4,aot);
-        filmy.put(5,paprika);
+        ArrayList<Kangur> kangury = new ArrayList<>();
+        for (int i = 1; i < 15; i++) {
+            kangury.add(new Kangur(i));
+        }
+        Iterator<Kangur> kangurIterator = kangury.iterator();
 
-
-        for(Map.Entry<Integer, Film> entry : filmy.entrySet()) {
-            System.out.println(entry.toString());
+        while (kangurIterator.hasNext()){
+            System.out.println(kangurIterator.next().skok());
         }
 
+        HashMap<String, Kangur> kanguryHash = new HashMap<>();
+        kanguryHash.put("Grzes",kangury.get(0));
+        kanguryHash.put("Marek",kangury.get(1));
+        kanguryHash.put("Agata",kangury.get(2));
+        kanguryHash.put("Marysia",kangury.get(3));
+        kanguryHash.put("Pawel",kangury.get(4));
+        kanguryHash.put("Piotr",kangury.get(5));
+        kanguryHash.put("Kacper",kangury.get(6));
+        kanguryHash.put("Oliwia",kangury.get(7));
+        kanguryHash.put("Bartek",kangury.get(8));
+        kanguryHash.put("Kuba",kangury.get(9));
+        kanguryHash.put("Karolina",kangury.get(10));
+        kanguryHash.put("Ola",kangury.get(11));
+        kanguryHash.put("Dominik",kangury.get(12));
+        kanguryHash.put("Romek",kangury.get(13));
 
+        Set<String> key = kanguryHash.keySet();
+        Iterator<String> hashIterator = key.iterator();
+
+
+        while (hashIterator.hasNext()){
+            String n = hashIterator.next();
+            System.out.println(n+" "+kanguryHash.get(n).skok());
+        }
+        LinkedHashMap<String,Kangur> posortowane = new LinkedHashMap<>(kanguryHash);
+        ArrayList<String> sortedkeys = new ArrayList<>(kanguryHash.keySet());
+        Collections.sort(sortedkeys);
+
+        for (String klucz: sortedkeys) {
+            System.out.println(klucz+kanguryHash.get(klucz).skok());
+            posortowane.put(klucz,kanguryHash.get(klucz));
+        }
 
     }
 }
